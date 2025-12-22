555a4aba155bd33316a613ebca3454d86dcbc4a0
zak <za444@proton.me>
2025-12-22 23:43:14 +0100

    project init

---
+++ Atk.Cis.Worker.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Atk.Cis.Worker.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Atk.Cis.Worker/Program.cs
@@ -0,0 +1,7 @@
+using Atk.Cis.Worker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
