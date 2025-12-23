84b9c09a56ac3cea1a765223f8fea9d7e4c95146
zak <za444@proton.me>
2025-12-23 03:16:46 +0100

    worker does process inputs correctly

---
+++ Atk.Cis.Worker/CheckInDesk.cs
@@ -0,0 +1,9 @@
+using Atk.Cis.Worker.Enums;
+using Atk.Cis.Worker.Models;
+
+namespace Atk.Cis.Worker;
+
+public class CheckInDesk
+{
+
+}
--- Atk.Cis.Worker/Worker.cs
+++ Atk.Cis.Worker/Worker.cs
@@ -11,13 +11,56 @@ public class Worker : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        _logger.LogInformation("ATK - Check-In System");
+
         while (!stoppingToken.IsCancellationRequested)
         {
             if (_logger.IsEnabled(LogLevel.Information))
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             }
+
+            var input = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(input))
+                continue;
+
+            if (input.StartsWith(":"))
+            {
+                HandleCommand(input[1..], stoppingToken);
+            }
+            else
+            {
+                // handle barcode
+                Console.WriteLine($"Barcode: {input}");
+            }
+
+
             await Task.Delay(1000, stoppingToken);
+        }
+    }
+
+    private void HandleCommand(string command, CancellationToken token)
+    {
+        switch (command)
+        {
+            case "status":
+                _logger.LogInformation("Worker alive.");
+                break;
+
+            case "quit":
+            case "exit":
+                _logger.LogInformation("Shutdown requested.");
+                Environment.Exit(0);
+                break;
+
+            case "help":
+                Console.WriteLine(":status | :help | :quit");
+                break;
+
+            default:
+                _logger.LogWarning("Unknown command: {Command}", command);
+                break;
         }
     }
 }
