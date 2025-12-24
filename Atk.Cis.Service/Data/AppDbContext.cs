5d03873f5171051e37ce933f1067e413fa6a4292
zak <za444@proton.me>
2025-12-24 12:01:40 +0100

    is ok

---
+++ Atk.Cis.Service/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+using Atk.Cis.Service.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MiniEfSqlite.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users => Set<User>();
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    // Optional: better naming for SQLite
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().ToTable("Users");
+        base.OnModelCreating(modelBuilder);
+    }
+}
