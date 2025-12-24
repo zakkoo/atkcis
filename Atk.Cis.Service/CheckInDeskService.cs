18e4b3f8851ff1512af7736d2f83668642596981
zak <za444@proton.me>
2025-12-24 10:53:21 +0100

    looking good

--- Atk.Cis.Service/CheckInDeskService.cs
+++ Atk.Cis.Service/CheckInDeskService.cs
@@ -1,5 +1,7 @@
+using Atk.Cis.Service.Enums;
 using Atk.Cis.Service.Data;
 using Atk.Cis.Service.Interfaces;
+using Atk.Cis.Service.Models;
 
 namespace Atk.Cis.Service;
 
@@ -16,6 +18,40 @@ public class CheckInDeskService : ICheckInDeskService
     public async Task<string> CheckIn(string barcode)
     {
         var user = _DbContext.Users.SingleOrDefault(x => x.PrimaryCode.ToLower() == barcode.ToLower());
-        return $"{user?.DisplayName} checked in.";
+        var plusOne = _DbContext.Users.SingleOrDefault(x => x.PlusOneCode.ToLower() == barcode.ToLower());
+        if (user != null)
+        {
+            var checkInSession = _DbContext.CheckInSessions.FirstOrDefault(x => x.Status == SessionStatus.Open && x.UserId == user.Id);
+            if (checkInSession == null)
+            {
+                checkInSession = new CheckInSession
+                {
+                    SessionId = Guid.NewGuid(),
+                    UserId = user.Id,
+                    OpenedAt = DateTimeOffset.Now,
+                };
+                _DbContext.CheckInSessions.Add(checkInSession);
+                return $"{user.DisplayName} checked in.";
+            }
+            checkInSession.Status = SessionStatus.Closed;
+            checkInSession.ClosedAt = DateTimeOffset.Now;
+            return $"{user.DisplayName} checked out.";
+        }
+        else if (plusOne != null)
+        {
+            return CheckInPlusOne(plusOne);
+        }
+        else
+        {
+            return "Invalid barcode. Check-in was cancelled.";
+        }
     }
+
+    private string CheckInUser(User user)
+    {
+
+        return null;
+    }
+    private string CheckInPlusOne(User plusOne) { return null; }
+
 }
