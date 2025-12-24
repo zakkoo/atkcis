2cc18b399efc7f7e4e8f0dbbbc0bae937d26afe6
zak <za444@proton.me>
2025-12-24 17:44:44 +0100

    not working yet

--- Atk.Cis.Worker/Program.cs
+++ Atk.Cis.Worker/Program.cs
@@ -1,11 +1,17 @@
 using Atk.Cis.Worker;
 using Atk.Cis.Service.Interfaces;
 using Atk.Cis.Service;
+using Atk.Cis.Service.Data;
 
+using Microsoft.EntityFrameworkCore;
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 
 builder.Services.AddScoped<ICheckInDeskService, CheckInDeskService>();
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var host = builder.Build();
 host.Run();
