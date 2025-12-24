b76992f9fe490fad758dacb32f5ba0cabd2892a5
zak <za444@proton.me>
2025-12-24 21:09:44 +0100

    db is finally here

---
+++ Atk.Cis.Service/Migrations/20251224200101_InitialCreate.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Atk.Cis.Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CheckInSessions",
+                columns: table => new
+                {
+                    SessionId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false),
+                    OpenedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: false),
+                    ClosedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                    PartnerCount = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CheckInSessions", x => x.SessionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    DisplayName = table.Column<string>(type: "TEXT", nullable: true),
+                    PrimaryCode = table.Column<string>(type: "TEXT", nullable: false),
+                    PlusOneCode = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CheckInSessions");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # ATK Check-in System
 
 This is a very simple check-in system.
+
+## Installation
+
+dotnet ef database update -p Atk.Cis.Service -s Atk.Cis.Worker
+
