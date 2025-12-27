aba62247b5e65ec69d10eb720060ca75337690f4
zakkoo <90413051+zakkoo@users.noreply.github.com>
2025-12-27 21:22:16 +0100

    V0.3.3
    * yml
    
    * logger works to rolling daily file
    
    * remove .vscode from tracking
    
    * .vscode
    
    * Format artifact names with ref prefix (#14)

--- Atk.Cis.Worker/Program.cs
+++ Atk.Cis.Worker/Program.cs
@@ -1,7 +1,26 @@
+using Serilog;
+using Serilog.Events;
 using Atk.Cis.Worker;
 using Atk.Cis.Service;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.File(
+        path: "logs/app-.log",
+        rollingInterval: RollingInterval.Day,
+        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}"
+    )
+    .WriteTo.Console()
+    .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(dispose: true);
+
+
 builder.Services.AddHostedService<Worker>();
 
 builder.Services.AddCisServices(builder.Configuration);
