b15160f1da7a354557076de94145d4184d75338a
zakkoo <90413051+zakkoo@users.noreply.github.com>
2025-12-29 22:38:03 +0100

    Feature/tailwind (#33)
    * node gitignore
    
    * almost there
    
    * Fix Tailwind CSS pipeline and layout stylesheet reference (#31)
    
    * Fix Tailwind build wiring and config
    
    * Update .gitignore to remove Node/npm ignores
    
    Remove Node/npm related entries from .gitignore
    
    * Remove redundant Tailwind theme tokens (#32)
    
    * done.

---
+++ Atk.Cis.Web/tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./Pages/**/*.cshtml", "./Views/**/*.cshtml"],
+  theme: {
+    extend: {
+      fontFamily: {
+        terminal: [
+          "ui-monospace",
+          "SFMono-Regular",
+          "Menlo",
+          "Monaco",
+          "Consolas",
+          '"Liberation Mono"',
+          '"Courier New"',
+          "monospace",
+        ],
+      },
+      colors: {
+        terminal: {
+          glow: "#00ff9c",
+        },
+      },
+    },
+  },
+};
